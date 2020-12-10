90245174f30fd571fc556b7392fb6a29a5a599e0
charankanumuri <charan.kanumuri@gmail.com>
2020-12-10 22:13:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+Amplify.configure(awsconfig);
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>SAI HI!!!!!!!</h1>
+			</div>
+		);
+	}
+}
+
+export default withAuthenticator(App);
